00b28812a717fe8589edd9e0eadfba98284fcfed
Rana Qasim <ranamqasim440@gmail.com>
2025-09-03 06:44:00 +0800

    SharedPreferences and Hive DB practiced with CRUD operations

---
+++ android/app/src/main/kotlin/com/example/flutter_local_db_practice_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_local_db_practice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_db_practice_app/models/user.dart';
+import 'package:flutter_local_db_practice_app/providers/hive_provider.dart';
+import 'package:flutter_local_db_practice_app/providers/shared_prefs_provider.dart';
+import 'package:flutter_local_db_practice_app/screens/home_screen.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserAdapter());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => SharedPreferencesProvider()..loadUsername(),
+        ),
+        ChangeNotifierProvider(create: (_)=>HiveProvider()..loadUsers()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: 'Local DB Practices',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const HomeScreen());
+  }
+}
